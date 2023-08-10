1743f26f2ed133726908d2d6809e75013e97123f
aayushgelal <aayushgelal@gmail.com>
2023-08-10 23:22:02 +0545

    video

--- README.md
+++ README.md
@@ -1,8 +1,5 @@
-## Full stack assignment
+This is a leetcode clone web app. We are given a few problems and a place to write the code. When we submit the code, it is sent to CHATGPT to check whether the written code is correct or not. According to that ChatGpt provides us the solution for the problem.
 
-### Video
+Note: I have not focused on UI. It is more about functionality than UI. But according to the time you are watching this project, The ChatGpt API might have been expired. So I have a video on how its working in the current time. Also i am using the free server so it might be slow. It will take some time to load.
 
-Link to video - https://www.youtube.com/watch?v=569YZm0X5-0
-
-### Where to start?
-Look at App.jsx inside the src/ folder 
+site link: https://leetcode-clone.onrender.com
---
+++ leetcode/src/Components/Shared/AlertCard.jsx
@@ -0,0 +1,23 @@
+import React from "react";
+import { AiFillCloseCircle } from "react-icons/ai";
+import ButtonClick from "./ButtonClick";
+
+export default function AlertCard({ message, handleClick }) {
+  return (
+    <div className="fixed top-0 -z-10 h-full w-full  flex items-center justify-center">
+      <div className="m-10 h-fit shadow-lg bg-gray-100 border-none font-bold p-10 align-middle">
+        <p className="font-bold"> The Answer is:</p>
+        <br></br>
+        <p className=" font-medium text-center">{message} </p>
+        <div className="flex items-center justify-center mt-5">
+          <button
+            onClick={handleClick}
+            className=" transition-transform hover:bg-sky-800 bg-sky-950 text-white p-2 rounded-3xl px-10"
+          >
+            Close
+          </button>
+        </div>
+      </div>
+    </div>
+  );
+}
